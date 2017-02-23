22ca7b390f9dc88ece0a2b908be6a55ebc88be9b
jaredthecoder <jared@jaredsmith.io>
2017-02-23 18:51:31 -0500

    Remove prompt_toolkit and downgrade to python 2, still broken when running mkfs, we can simplify this like they said in class

--- bastion/validators.py
+++ bastion/validators.py
@@ -3,41 +3,33 @@
 """
 
 
-from prompt_toolkit.validation import Validator, ValidationError
-
 
 yn_answers = ['y', 'n', 'yes', 'no']
 commands = ['mkfs', 'open', 'ls', 'read', 'write', 'seek', 'close',
             'mkdir', 'rmdir', 'tree', 'cd', 'cat', 'import', 'export']
 
 
-class YesNoValidator(Validator):
 
-    def validate(self, document):
-        text = document.text
-
-        if text:
-            if text not in yn_answers:
-                raise ValidationError(message='This input is not yes, no, y, or n.',
-                                      cursor_position=0)
+def validate_yes_no(text):
+    if text:
+        if text not in yn_answers:
+            print('This input is not yes, no, y, or n.')
+            return False
+        return True
 
 
-class CommandValidator(Validator):
-    def validate(self, document):
-        text = document.text
-
-        if text:
-            if text not in commands:
-                raise ValidationError(message='This input is not a valid command.',
-                                      cursor_position=0)
+def validate_command(text):
+    if text:
+        if text not in commands:
+            print('This input is not a valid command.')
+            return False
+        return True
 
 
-class MkfsValidator(Validator):
-    def validate(self, document):
-        text = document.text
-
-        if text:
-            if text != 'mkfs':
-                raise ValidationError(message='Please call mkfs when the file system does not yet exist.',
-                                      cursor_position=0)
+def validate_mkfs(text):
+    if text:
+        if text != 'mkfs':
+            print('Please call mkfs when the file system does not yet exist')
+            return False
+        return True
 
